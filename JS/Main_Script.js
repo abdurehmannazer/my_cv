8bcab67ada41f23c72ead83bbc8ff8cc808750db
Abdurehman_Nazari <abdurehmannazer@gmail.com>
2023-06-02 19:27:40 +0300

    a

--- JS/Main_Script.js
+++ JS/Main_Script.js
@@ -1,2 +1,10 @@
 console.log("Hi")
 
+var bar_button = document.querySelector(".navbar-toggler");
+var bar_icon = document.querySelector(".navbar-toggler i");
+
+
+
+bar_button.onclick = function () {
+    bar_icon.classList.toggle("fa-bars-staggered")
+}
