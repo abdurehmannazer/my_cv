88596cfe9f4b84aa00999128881819fa7d1e5647
Abdurehman_Nazari <abdurehmannazer@gmail.com>
2023-06-02 19:48:17 +0300

    szdev

--- JS/Main_Script.js
+++ JS/Main_Script.js
@@ -6,5 +6,14 @@ var bar_icon = document.querySelector(".navbar-toggler i");
 
 
 bar_button.onclick = function () {
-    bar_icon.classList.toggle("fa-bars-staggered")
+    bar_icon.classList.toggle("fa-bars-staggered");
+    if (bar_icon.classList.contains("anima")) {
+        bar_icon.classList.remove("anima")
+    } else {
+        bar_icon.classList.add("anima")
+
+    }
+
+
+
 }
